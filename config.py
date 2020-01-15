c376a2465495a5a407f2f055ff3d1a6f10254ec9
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-15 09:39:05 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mutumas:Mutuma1234@localhost/pitch'
+    
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -15,6 +16,35 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
     
     
 
